4ad3b452db76303a16f69564e7934a77a3550b1f
pallavipatil1233 <127382370+pallavipatil1233@users.noreply.github.com>
2025-02-12 17:10:39 +0530

    Delete Back-End directory

--- Back-End/Rent-A-Ride/src/main/java/com/rentalcar/dao/CarDao.java
+++
@@ -1,9 +0,0 @@
-package com.rentalcar.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rentalcar.pojos.Car;
-
-public interface CarDao extends JpaRepository<Car, Long> {
-
-}
